1b58d39f5a886a3e3e816609fccc84673c0fdc6e
syahdamzha_ <syahdamfacc12@gmail.com>
2024-04-28 13:32:29 +0700

    Fungsi findword & int main update

--- Petruk Find Word.cpp
+++ Petruk Find Word.cpp
@@ -1,7 +1,19 @@
-#include <iostrean>
+#include <iostream>
+#include <string>
+
 using namespace std;
-int main(){
-	cout<<"TEST 22";
-	
-	cout<<"TEST SANDRA";
+
+bool findWord(const string* matriks, int Baris, int Kolom, const string& kata) {
+
+    }
+    
+int main() {
+
+    int jumlahKata;
+    cin >> jumlahKata;
+
+    string* kata = new string[jumlahKata];
+    for (int i = 0; i < jumlahKata; ++i) {
+        cin >> kata[i];
+    }
 }
